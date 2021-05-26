8c27ec4030c37d59edeb8366f7c7367de1160250
BrookJeynes <jeynesbrook@gmail.com>
2021-05-26 18:11:57 +1000

    added html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return "Work Tracker"
+  return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0')
