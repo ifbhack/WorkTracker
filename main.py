ecdeb7a75eecfe0ea79990673c4ac4eaad86893d
BrookJeynes <jeynesbrook@gmail.com>
2021-06-07 19:42:31 +1000

    added manager/employee differenciator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,15 @@ def index():
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
+  if request.method == 'POST':
+    name, password = request.form['name'], request.form['password']
+    
+    if name[0].upper() == 'E':
+      print(f'Employee with the name: {name[2:]}')
+    elif name[0].upper() == 'M':
+      print(f'Manager with the name: {name[2:]}')
+        
+
   return render_template('index-login.html')
 
 if __name__ == '__main__':
