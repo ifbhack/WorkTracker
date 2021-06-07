ecf2cbb554aae1e2c30b44c71805b7bc35590376
BrookJeynes <jeynesbrook@gmail.com>
2021-06-07 15:07:03 +1000

    added first webpage, adding more now

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+  return render_template('index-login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
