30753fae9f5f5980ed92f2c0416498bdd5579e97
Antoni <81144056+azazulak@users.noreply.github.com>
2021-06-08 18:16:51 +1000

    Update work_tracker.sql

--- work_tracker.sql
+++ work_tracker.sql
@@ -38,14 +38,29 @@ CREATE TABLE IF NOT EXISTS Roster (
 ); 
 
 # Sample data
-INSERT INTO Payrate (roleName, payRate) VALUES ("Floor Worker", 2.80), ("CEO", 100.01);
+INSERT INTO Payrate (roleName, payRate) VALUES ("Floor Worker", 2.80), 
+	("CEO", 100.01),
+    ('Window Cleaning', 5.76),
+    ('CFO', 82.9),
+    ('Shelf Stocker', 14.21),
+    ('Store Manager', 30.50);
 
 INSERT INTO Staff (roleName, name, password, isManager)
 VALUES ("Floor Worker", "Joe Smith", "Im not joe smith", False),
-("CEO", "Chad Chadwitch", "yes.itisI", True);
+("CEO", "Chad Chadwitch", "yes.itisI", True),
+("Store Manager", "Cool Bill", "eatmy***", True),
+("Shelf Stocker", "Hugo Phillips", "Ilovepeople", False);
 
-INSERT INTO Workplace (managerID) VALUES (2);  # Chad Chadwitch
+INSERT INTO Workplace (managerID) VALUES (2), (3);  # Chad Chadwitch
 
 INSERT INTO Roster (staffID, date, workplaceID, duration)
 VALUES (1, "2021-06-08", 1, 4),  # Joe Smith
-(2, "2021-06-09", 1, 10);  # Chad Chadwitch
+(2, "2021-06-09", 1, 10),
+(2, "2021-06-10", 1, 8),
+(2, "2021-06-11", 1, 4),
+(2, "2021-06-12", 1, 6),
+(2, "2021-06-13", 1, 9),
+(2, "2021-06-14", 2, 11),
+(2, "2021-06-15", 1, 2),
+(2, "2021-06-16", 1, 5),
+(3, "2021-06-14", 2, 9);  # Chad Chadwitch
