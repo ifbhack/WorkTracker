940dbd7d31ed9d030b82b3b3d1744e4531bb7be6
owu <83254308+owu-1@users.noreply.github.com>
2021-07-11 15:39:50 +1000

    Employee create/view back-end

--- work_tracker/controllers/staff.py
+++ work_tracker/controllers/staff.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 
 bp = Blueprint("staff", __name__, url_prefix="/staff")
 
+
 @bp.route("/view", methods=["GET"])
 def view():
     # TODO: add try/catch, for now fail hard
@@ -24,13 +25,39 @@ def create():
             request.form['fname'],
             request.form['lname'],
             request.form['eaddress'],
-            "password",
             request.form['contact'],
             request.form['address1'] + ' ' + request.form['address2'],
             request.form['suburb'],
             request.form['postcode'],
             request.form['state'],
-            "2016-08-30",
-            False)
+            request.form['dob'],
+            request.form['level'])
+
+    return render_template('index-create-employee.html')
+
+
+@bp.route("/edit", methods=["GET", "POST"])
+def edit():
+    # NOTE: Hack to get g.user working from view function
+    staffMembers = g.staffModel.getStaffMembers()
+    g.user = staffMembers[0]
+
+    # TODO: Edit other employees other than g.user
+    # TODO: On submit, data is changed but fields contain old data
+    if request.method == 'POST':
+        staffMember = g.staffModel.getStaffMember(g.user.staffID)
+
+        staffMember.firstName = request.form['fname']
+        staffMember.lastName = request.form['lname']
+        staffMember.email = request.form['eaddress']
+        staffMember.contactNumber = request.form['contact']
+        staffMember.address = request.form['address1']
+        staffMember.suburb = request.form['suburb']
+        staffMember.postcode = request.form['postcode']
+        staffMember.state = request.form['state']
+        staffMember.dob = request.form['dob']
+        staffMember.isManager = request.form['level']
+
+        g.staffModel.updateStaffMember(staffMember)
 
     return render_template('index-edit-employee.html')
