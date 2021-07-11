55984d4e5f2effa83bcbef1e7feb2a8df7d7a5b9
owu <83254308+owu-1@users.noreply.github.com>
2021-07-11 13:34:57 +1000

    Update SQL scripts
    Added password column
    Easier process to update to new db

--- work_tracker/schema.sql
+++ work_tracker/schema.sql
@@ -1,46 +1,53 @@
-CREATE TABLE "Availability" (
-    "staffID"    TEXT NOT NULL,
-    "startTime"    TEXT NOT NULL,
-    "duration"    TEXT NOT NULL,
-    PRIMARY KEY("staffID","startTime"),
-    FOREIGN KEY("staffID") REFERENCES "Staff"("staffID")
+DROP TABLE IF EXISTS "Roster";
+DROP TABLE IF EXISTS "Availability";
+DROP TABLE IF EXISTS "Staff";
+DROP TABLE IF EXISTS "Payroll_Query";
+DROP TABLE IF EXISTS "Payroll_Query_Details";
+
+CREATE TABLE IF NOT EXISTS "Roster" (
+	"staffID"	INTEGER NOT NULL,
+	"startTime"	TEXT NOT NULL,
+	"duration"	INTEGER NOT NULL,
+	FOREIGN KEY("staffID") REFERENCES "Staff"("staffID"),
+	PRIMARY KEY("staffID")
 );
 
-CREATE TABLE "Payroll_Query" (
-    "payrollID"    INTEGER,
-    "staffID"    INTEGER,
-    "date"    TEXT,
-    "status"    INTEGER,
-    FOREIGN KEY("staffID") REFERENCES "Staff"("staffID"),
-    PRIMARY KEY("payrollID" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS "Availability" (
+	"staffID"	TEXT NOT NULL,
+	"startTime"	TEXT NOT NULL,
+	"duration"	TEXT NOT NULL,
+	FOREIGN KEY("staffID") REFERENCES "Staff"("staffID"),
+	PRIMARY KEY("staffID","startTime")
 );
 
-CREATE TABLE "Payroll_Query_Details" (
-    "payrollID"    INTEGER,
-    "startTime"    INTEGER,
-    "duration"    INTEGER,
-    FOREIGN KEY("payrollID") REFERENCES "Payroll_Query"("staffID")
+CREATE TABLE IF NOT EXISTS "Staff" (
+	"staffID"	INTEGER NOT NULL UNIQUE,
+	"firstName"	TEXT NOT NULL,
+	"lastName"	NUMERIC NOT NULL,
+	"email"	TEXT NOT NULL UNIQUE,
+	"password"	TEXT NOT NULL,
+	"contactNumber"	INTEGER NOT NULL,
+	"address"	TEXT NOT NULL,
+	"suburb"	TEXT NOT NULL,
+	"postcode"	INTEGER NOT NULL,
+	"state"	TEXT NOT NULL,
+	"dob"	TEXT NOT NULL,
+	"isManager"	INTEGER NOT NULL,
+	PRIMARY KEY("staffID" AUTOINCREMENT)
 );
 
-CREATE TABLE "Roster" (
-    "staffID"    INTEGER NOT NULL,
-    "startTime"    TEXT NOT NULL,
-    "duration"    INTEGER NOT NULL,
-    FOREIGN KEY("staffID") REFERENCES "Staff"("staffID"),
-    PRIMARY KEY("staffID")
+CREATE TABLE IF NOT EXISTS "Payroll_Query" (
+	"payrollID"	INTEGER NOT NULL,
+	"staffID"	INTEGER NOT NULL,
+	"date"	TEXT NOT NULL,
+	"status"	INTEGER NOT NULL,
+	FOREIGN KEY("staffID") REFERENCES "Staff"("staffID"),
+	PRIMARY KEY("payrollID" AUTOINCREMENT)
 );
 
-CREATE TABLE "Staff" (
-    "staffID"    INTEGER NOT NULL UNIQUE,
-    "firstName"    TEXT NOT NULL,
-    "lastName"    NUMERIC NOT NULL,
-    "email"    TEXT NOT NULL UNIQUE,
-    "contactNumber"    INTEGER NOT NULL,
-    "address"    TEXT NOT NULL,
-    "suburb"    TEXT NOT NULL,
-    "postcode"    INTEGER NOT NULL,
-    "state"    TEXT NOT NULL,
-    "dob"    TEXT NOT NULL,
-    "isManager"    INTEGER NOT NULL,
-    PRIMARY KEY("staffID" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS "Payroll_Query_Details" (
+	"payrollID"	INTEGER NOT NULL,
+	"startTime"	INTEGER NOT NULL,
+	"duration"	INTEGER NOT NULL,
+	FOREIGN KEY("payrollID") REFERENCES "Payroll_Query"("staffID")
 );
